19b2094d4ca6bdea589f958fd4d7427a0b01f6d4
LMCMLJ <mjcalvey2@gmail.com>
2016-12-08 23:52:11 +0000

    Getting ready to add tris

--- road-generator.js
+++ road-generator.js
@@ -18,9 +18,8 @@ function generateSegment(points, i) {
   segment.normalRight = [dy, -1 * dx];
   segment.left = [[points[i][0] + segment.normalLeft[0], points[i][1] + segment.normalLeft[1]], [points[i+1][0] + segment.normalLeft[0], points[i+1][1] + segment.normalLeft[1]]];
   segment.right = [[points[i][0] + segment.normalRight[0], points[i][1] + segment.normalRight[1]], [points[i+1][0] + segment.normalRight[0], points[i+1][1] + segment.normalRight[1]]];
-  console.log(segment);
-  console.log();
+  segment.tris = [];
   return segment;
 }
 
-generate([[0, 0], [0, 1], [0, 3], [0, 7]]);
+console.log(generate([[0, 0], [0, 1], [0, 3], [0, 7]]));
