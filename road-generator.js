a78d2f067f20122ab67dc1e11fb9b70c51c2a846
LMCMLJ <mjcalvey2@gmail.com>
2016-12-09 23:16:33 +0000

    Script now properly generates all segments, and adjusts triangle positions and denotations.

--- road-generator.js
+++ road-generator.js
@@ -2,6 +2,8 @@ function generate (points) {
   var road = {};
   road.points = points;
   road.segments = generateSegments(points);
+  road.vertices = generateVertices(road.segments);
+  road.triangles = generateTriangles(road.segments);
   return road;
 }
 
@@ -30,4 +32,30 @@ function generateSegment(points, i) {
   return segment;
 }
 
-console.log(generate([[0, 0], [0, 1], [1, 2], [2, 4]]).segments[1]);
+function generateVertices(segments) {
+  var vertices = [];
+  for(i = 0; i < segments.length; i++) {
+    var newVertices = [];
+    var oldVertices = segments[i].vertices;
+    for(x = 0; x < oldVertices.length; x++) {
+      newVertices.push([oldVertices[x][0] * (i + 1), oldVertices[x][1] * (i + 1)]);
+    }
+    vertices.push(newVertices);
+  }
+  return vertices;
+}
+
+function generateTriangles(segments) {
+  var triangles = [];
+  for(i = 0; i < segments.length; i++) {
+    var newTriangles = [];
+    var oldTriangles = segments[i].triangles;
+    for(x = 0; x < oldTriangles.length; x++) {
+      newTriangles.push(oldTriangles[x] * (i + 1));
+    }
+    triangles.push(newTriangles);
+  }
+  return triangles;
+}
+
+generate([[0, 0], [0, 1], [1, 2], [2, 4]]);
