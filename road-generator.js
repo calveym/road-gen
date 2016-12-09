0b459dcd4c603831ff68bff71e33b395bc1a62f4
LMCMLJ <mjcalvey2@gmail.com>
2016-12-09 22:43:23 +0000

    Segments generating properly

--- road-generator.js
+++ road-generator.js
@@ -1,12 +1,16 @@
 function generate (points) {
   var road = {};
   road.points = points;
-  road.segments = [];
-
-  for(i = 0; i < points.length - 1; i++) {
-    road.segments.push(generateSegment(road.points, i));
-  }
+  road.segments = generateSegments(points);
   return road;
+}
+
+function generateSegments(points) {
+  var segments = [];
+  for(i = 0; i < points.length - 1; i++) {
+    segments.push(generateSegment(points, i));
+  }
+  return segments;
 }
 
 function generateSegment(points, i) {
@@ -18,8 +22,12 @@ function generateSegment(points, i) {
   segment.normalRight = [dy, -1 * dx];
   segment.left = [[points[i][0] + segment.normalLeft[0], points[i][1] + segment.normalLeft[1]], [points[i+1][0] + segment.normalLeft[0], points[i+1][1] + segment.normalLeft[1]]];
   segment.right = [[points[i][0] + segment.normalRight[0], points[i][1] + segment.normalRight[1]], [points[i+1][0] + segment.normalRight[0], points[i+1][1] + segment.normalRight[1]]];
-  segment.tris = [];
+  segment.vertices = segment.left.concat(segment.center.concat(segment.right));
+  segment.triangles = [2, 0, 1,
+                       2, 1, 3,
+                       2, 3, 4,
+                       4, 3, 5];
   return segment;
 }
 
-console.log(generate([[0, 0], [0, 1], [0, 3], [0, 7]]));
+console.log(generate([[0, 0], [0, 1], [1, 2], [2, 4]]).segments[1]);
