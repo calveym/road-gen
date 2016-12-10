59f0f01d2b79676db6e932b5afea19a94cb772fe
LMCMLJ <mjcalvey2@gmail.com>
2016-12-10 23:28:08 +0000

    Ported output to p5

--- road-generator.js
+++ road-generator.js
@@ -1,11 +1,31 @@
+var newRoad;
+var canvas;
+var frame = 0;
+var roadDraw = [];
+
 function generate (points) {
   var road = {};
   road.points = points;
   road.segments = generateSegments(points);
   road.vertices = generateVertices(road.segments);
+  road.vertices = road.vertices.reduce(function(a, b) {
+    return a.concat(b);
+  }, []);
   road.triangles = generateTriangles(road.segments);
-  console.log(road);
+  printTriangles(road);
   return road;
+}
+
+function printTriangles(road) {
+  for(i = 0; i < (road.triangles.length); i+=3) {
+    console.log(road.vertices[road.triangles[i]][0] * 20);
+    triangle((road.vertices[road.triangles[i]][0]) * 20,
+             (road.vertices[road.triangles[i]][1]) * 20,
+             (road.vertices[road.triangles[i + 1]][0]) * 20,
+             (road.vertices[road.triangles[i + 1]][1]) * 20,
+             (road.vertices[road.triangles[i + 2]][0]) * 20,
+             (road.vertices[road.triangles[i + 2]][1]) * 20);
+  }
 }
 
 function generateSegments(points) {
@@ -26,10 +46,15 @@ function generateSegment(points, i) {
   segment.left = [[points[i][0] + segment.normalLeft[0], points[i][1] + segment.normalLeft[1]], [points[i+1][0] + segment.normalLeft[0], points[i+1][1] + segment.normalLeft[1]]];
   segment.right = [[points[i][0] + segment.normalRight[0], points[i][1] + segment.normalRight[1]], [points[i+1][0] + segment.normalRight[0], points[i+1][1] + segment.normalRight[1]]];
   segment.vertices = segment.left.concat(segment.center.concat(segment.right));
+  console.log(segment.vertices);
   segment.triangles = [2, 0, 1,
                        2, 1, 3,
                        2, 3, 4,
                        4, 3, 5];
+  segment.p5TrianglesStacked = segment.left.concat(segment.center.concat(segment.right));
+  segment.p5Triangles = segment.p5TrianglesStacked.reduce(function(a, b) {
+    return a.concat(b);
+  }, []);
   return segment;
 }
 
@@ -38,8 +63,9 @@ function generateVertices(segments) {
   for(i = 0; i < segments.length; i++) {
     var newVertices = [];
     var oldVertices = segments[i].vertices;
-    for(x = 0; x < oldVertices.length; x++) {
-      newVertices.push([oldVertices[x][0] * (i + 1), oldVertices[x][1] * (i + 1)]);
+    for(x = 1; x <= oldVertices.length; x++) {
+      newVertices.push([oldVertices[x - 1][0],
+                        oldVertices[x - 1][1]]);
     }
     vertices.push(newVertices);
   }
@@ -52,11 +78,22 @@ function generateTriangles(segments) {
     var newTriangles = [];
     var oldTriangles = segments[i].triangles;
     for(x = 0; x < oldTriangles.length; x++) {
-      newTriangles.push(oldTriangles[x] * (i + 1));
+      newTriangles.push(oldTriangles[x] + (i * 6));
     }
     triangles = triangles.concat(newTriangles);
   }
   return triangles;
 }
 
-generate([[0, 0], [0, 1], [1, 2], [2, 4]]);
+
+
+function setup() {
+  canvas = createCanvas(1680, 900);
+}
+
+function draw() {
+  canvas.clear();
+  frame++;
+  roadDraw.push([frame * 2 - 1.9, Math.sin(Math.cos(frame * 0.125)) * 5 + 20]);
+  newRoad = generate(roadDraw);
+}
